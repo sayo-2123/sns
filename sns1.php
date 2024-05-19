170e11195635b6f14d4d511d4dd9b815b5a499c3
sayo-2123 <classa1990@gmail.com>
2024-05-19 16:57:14 +0900

    送信フォームの実装

---
+++ sns1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="sns2.php" method="post">
+    名前
+    <div><input type="text" name="n"></div>
+    メッセージ
+    <div><textarea name="m"></textarea></div>
+    <input type="submit" value="送信する">
+  </form>
+
+
+
+</body>
+
+</html>
